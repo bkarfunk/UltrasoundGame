8d56af51fb12978aa74cb6976f94b4d83e7794a5
Tom Fadial <tomfadial@gmail.com>
2018-12-11 09:43:14 -0600

    Added react-router

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default class Home extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<h2>Home</h2>
+				<Link to="/GallBladder/">Gallbladder</Link>
+			</div>
+		)
+	}
+}
