3f76c49eea9118418af219c9f12e6811f8f18a2f
Tom Fadial <tomfadial@gmail.com>
2018-12-11 10:10:36 -0600

    Add measure package and initial route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './Home';
 import GallBladder from './GallBladder';
+import Measure from './Measure';
 
 
 export default class App extends React.Component {
@@ -12,6 +13,7 @@ export default class App extends React.Component {
 				<div>
 		      <Route component={Home} />
 		      <Route path="/GallBladder/" component={GallBladder} />
+		      <Route path="/Measure/" component={Measure} />
 				</div>
 		  </Router>
 		)
---
+++ src/Measure.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MeasurementLayer, calculateDistance, calculateArea } from 'react-measurements';
+import normal from './Images/GB/Normal.png';
+
+export default class Measure extends React.Component {
+	render () {
+		return (
+			<h2>Measure</h2>
+		)
+	}
+}
