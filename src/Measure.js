952a70b5429e28bef47adaf378c107db878ebb65
Tom Fadial <tomfadial@gmail.com>
2018-12-11 10:27:50 -0600

    Working measurement

--- src/Measure.js
+++ src/Measure.js
@@ -4,9 +4,60 @@ import { MeasurementLayer, calculateDistance, calculateArea } from 'react-measur
 import normal from './Images/GB/Normal.png';
 
 export default class Measure extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			measurements: []
+		}
+	}
+
+  onChange = (measurements) => {
+		this.setState({ ...this.state, measurements })
+	}
+
+  measureLine = (line) => {
+		return Math.round(calculateDistance(line, 100, 100)) + " nanometers";
+	}
+
+  measureCircle = (circle) => {
+  	return Math.round(calculateArea(circle, 100, 100)) + " mm²";
+  }
+
+  onLoad = () => {
+    this.onImageBoundsChanged();
+  };
+
+  onImageBoundsChanged = (event) => {
+    const imageBounds = this.image.getBoundingClientRect();
+    this.setState({
+      widthInPx: imageBounds.width,
+      heightInPx: imageBounds.height
+    });
+  };
+
 	render () {
 		return (
-			<h2>Measure</h2>
+			<div 
+				style={{
+          position: 'absolute',
+          fontFamily: 'sans-serif',
+				}}
+			>
+				<img 
+	        ref={e => (this.image = e)}
+					src={normal} 
+          onLoad={this.onLoad}
+				/>
+		    <MeasurementLayer
+		      measurements={this.state.measurements}
+          widthInPx={this.state.widthInPx}
+          heightInPx={this.state.heightInPx}
+		      onChange={this.onChange}
+		      measureLine={this.measureLine}
+		      measureCircle={this.measureCircle}
+		    />
+			</div>
 		)
 	}
 }
